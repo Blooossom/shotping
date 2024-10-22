9cf9a9efa12567f820a8d190cb88205c844a190c
Blooossom <taegyeong19@gmail.com>
2024-10-22 13:34:14 +0900

    feat: restart project

---
+++ src/main/java/blooossom/shortping/ShortpingApplication.java
@@ -0,0 +1,13 @@
+package blooossom.shortping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blooossom/shortping/ShortpingApplicationTests.java
@@ -0,0 +1,13 @@
+package blooossom.shortping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
