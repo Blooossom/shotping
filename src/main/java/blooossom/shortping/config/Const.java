a71f67324795cd0e11ab1a1be8468cceb4903c37
Blooossom <taegyeong19@gmail.com>
2024-10-23 10:17:43 +0900

    feat: 상수 처리할 클래스 추가

---
+++ src/main/java/blooossom/shortping/config/Const.java
@@ -0,0 +1,14 @@
+package blooossom.shortping.config;
+
+import lombok.Data;
+
+@Data
+public class Const {
+
+    protected static final String SCHEMA = "shortping";
+
+    public static final String API_PRODUCES_JSON = "application/json";
+
+    public static final String API_CONSUMES_JSON = "application/json";
+
+}
