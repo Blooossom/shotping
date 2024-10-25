0759c0f50c08aeb8a71b999511bcfb0f4f7195e5
Blooossom <taegyeong19@gmail.com>
2024-10-25 10:43:22 +0900

    feat: 회원가입 페이지 라우팅 추가

--- shortping-front/src/main.tsx
+++ shortping-front/src/main.tsx
@@ -5,9 +5,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./pages/App.tsx";
 import Login from "./pages/Login.tsx";
+import SignUp from "./pages/SignUp.tsx";
 
 
 const queryClient = new QueryClient();
+// @ts-ignore
 const root = createRoot(document.getElementById("root"));
 
 root.render(
@@ -16,6 +18,7 @@ root.render(
       <Routes>
         <Route path="/main" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>,
